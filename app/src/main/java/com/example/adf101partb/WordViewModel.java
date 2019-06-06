afab37b219535236b7f2f98336d2615eaa1bda63
Ernest Philip GUEVARA <guevara-e@cyscorpions.com>
2019-06-06 14:48:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/adf101partb/WordViewModel.java
@@ -0,0 +1,26 @@
+package com.example.adf101partb;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+
+    public void deleteAll() {mRepository.deleteAll();}
+
+    public void deleteWord(Word word) {mRepository.deleteWord(word);}
+}
